604f33eff31151148759283f781e7b2222c7b097
Diógenes Dantas Lélis Júnior <diogjunior10071@gmail.com>
2022-02-19 22:47:13 -0300

    Alterando arquivo lerArquivo

--- lerArquivo.c
+++ lerArquivo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/*
 int main(void)
 {
  char url[]="tripadvisor_hotel_reviews.csv", //passando o arquivo para o vetor char
@@ -9,14 +9,40 @@ int main(void)
  FILE *arq;
   
  arq = fopen(url, "r"); //lendo o arquivo//
+ if(arq == NULL){
+   printf("Erro, nao foi possivel abrir o arquivo\n");
+ }
+
+ else{
+  while( (ch=fgetc(arq))!= '4' ){
+   //if(ch == '\n'){
+    printf("%c", ch);  
+     //num++;
+   //}
+  }
+ }
+ //printf("Existem %d linhas no arquivo\n", num);
+ fclose(arq);
+ 
+ return 0;
+}
+*/
+#include <stdio.h>
+
+int main(void)
+{
+ char url[]="tripadvisor_hotel_reviews.csv",
+      ch1;
+  int rating;
+ FILE *arq;
+ 
+ arq = fopen(url, "r");
  if(arq == NULL)
    printf("Erro, nao foi possivel abrir o arquivo\n");
  else
-  while( (ch=fgetc(arq))!= EOF )
-   if(ch == '2')
-     num++;
+  while( (fscanf(arq,"%c %d", &ch1, &rating))!='2' )
+   printf("%c %d\n", ch1, rating);
  
- printf("Existem %d linhas no arquivo\n", num);
  fclose(arq);
  
  return 0;
